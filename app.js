e676f4f5a79406b0a9d73b0b22a7fec84c8ff999
oicoru10 <46920229+oicoru10@users.noreply.github.com>
2021-10-05 13:53:47 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+//var indexRouter = require('./routes/index');
+var run01 = require('./run01');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', indexRouter);
+app.use('/run01', run01);
+
+module.exports = app;
